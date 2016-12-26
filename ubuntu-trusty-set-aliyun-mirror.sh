59c27f1ca9e76919476d6ce96ce82534d3da8011
周丞 <cheng@macOS.local>
2016-12-26 20:30:26 +0800

    add ubuntu-trusty-set-aliyun-mirror.sh

---
+++ ubuntu-trusty-set-aliyun-mirror.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# zhoucheng@ldsink.com
+set -e
+
+# backup current source list
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.$(date +%%Y%m%d%H%M%S).bak
+
+# set aliyun mirror
+sudo cat > /etc/apt/source.list << EOL
+deb http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-security main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
+deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
+EOL
