4f1dcb75cb6dc52b61550a0c1b015f8f52de4e47
ZHOU Cheng <c.zhou@live.com>
2016-12-27 14:57:52 +0800

    add ubuntu-install-nginx-stable.sh

---
+++ ubuntu-install-nginx-stable.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# zhoucheng@ldsink.com
+set -e
+
+. /etc/lsb-release
+
+# add the key used to sign the nginx packages and repository to the apt program keyring.
+wget https://nginx.org/keys/nginx_signing.key && sudo apt-key add nginx_signing.key && rm nginx_signing.key
+
+# backup current source list
+if [ -e "/etc/apt/sources.list.d/nginx.list" ]; then
+    sudo mv /etc/apt/sources.list.d/nginx.list /etc/apt/sources.list.d/nginx.list.$(date +%Y%m%d%H%M%S).bak
+fi
+
+# add nginx source list
+sudo cat > /etc/apt/sources.list.d/nginx.list << EOL
+deb http://nginx.org/packages/ubuntu/ $DISTRIB_CODENAME nginx
+deb-src http://nginx.org/packages/ubuntu/ $DISTRIB_CODENAME nginx
+EOL
+
+# install nginx stable version
+sudo apt update && sudo apt install nginx -y
