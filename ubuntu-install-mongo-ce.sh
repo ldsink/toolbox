7d6cf354436ca4e99dfcafdeac212848354178a0
ZHOU Cheng <zhoucheng@ldsink.com>
2017-01-16 12:28:12 +0800

    add ubuntu-install-mongo-ce.sh

---
+++ ubuntu-install-mongo-ce.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# zhoucheng@ldsink.com
+set -e
+
+MONGODB_VERSION="3.4"
+
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root"
+    exit
+fi
+
+# Import the public key used by the package management system.
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+
+# Create a list file for MongoDB.
+. /etc/lsb-release
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu $DISTRIB_CODENAME/mongodb-org/$MONGODB_VERSION multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+
+# Reload local package database.
+sudo apt-get update
+
+# Install the MongoDB packages.
+sudo apt-get install -y mongodb-org
+
+# Start MongoDB.
+sudo service mongod start
