f341019302301f21fa3dde731602a306c3490209
ZHOU Cheng <c.zhou@live.com>
2017-01-16 17:31:10 +0800

    wrap line

--- ubuntu-disable-transparent-hugepages.sh
+++ ubuntu-disable-transparent-hugepages.sh
@@ -20,7 +20,9 @@ sudo chmod 755 /etc/init.d/disable-transparent-hugepages
 # Configure your operating system to run it on boot.
 sudo update-rc.d disable-transparent-hugepages defaults
 
-echo "Success. Please reboot your system to make it works.\n"
+echo
+echo
+echo "Success. Please reboot your system to make it works."
 echo "You can check the status of THP support by issuing the following commands:"
 echo "    cat /sys/kernel/mm/transparent_hugepage/enabled"
 echo "    cat /sys/kernel/mm/transparent_hugepage/defrag"
