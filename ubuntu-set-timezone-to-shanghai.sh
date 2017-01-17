f62fc5959fe0a4de0ef173a982baa312157b4971
ZHOU Cheng <zhoucheng@ldsink.com>
2017-01-17 13:42:24 +0800

    add ubuntu-set-timezone-to-shanghai.sh

---
+++ ubuntu-set-timezone-to-shanghai.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# ZHOU Cheng <c.zhou@live.com>
+
+# List of tz database time zones
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+sudo timedatectl set-timezone Asia/Shanghai
