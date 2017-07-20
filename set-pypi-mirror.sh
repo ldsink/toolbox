1b056293ac744dd02cbb95d9fe6b1d6f03977f87
ZHOU Cheng <c.zhou@live.com>
2017-07-20 20:09:48 +0800

    feat: add set pypi mirror script

---
+++ set-pypi-mirror.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+# ZHOU Cheng <c.zhou@live.com>
+
+set -e
+
+mirror=""
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --mirror)
+            mirror="$2"
+            shift
+            ;;
+        *)
+            echo "Illegal option $1"
+            ;;
+    esac
+    shift $(( $# > 0 ? 1 : 0 ))
+done
+
+# default use aliyun mirror
+case "$mirror" in
+    douban)
+        host="pypi.douban.com"
+        url="https://pypi.douban.com/simple"
+        ;;
+    *)
+        host="mirrors.aliyun.com"
+        url="https://mirrors.aliyun.com/pypi/simple"
+        ;;
+esac
+
+mkdir -p ~/.pip
+cat > ~/.pip/pip.conf << EOF
+[global]
+trusted-host = ${host}
+index-url = ${url}
+EOF
