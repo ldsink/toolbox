2054f1ffd57fda8e79ff24f81c314a911f015f23
ZHOU Cheng <c.zhou@live.com>
2020-04-10 15:58:20 +0800

    update scripts

--- init-ubuntu-user-from-root.sh
+++ init-ubuntu-user-from-root.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Copy .ssh/authorized_keys to ubuntu user
-cp -R /root/.ssh/ /home/ubuntu/ && chown ubuntu:ubuntu -R /home/ubuntu/.ssh/
+cp -R /root/.ssh/ /home/ubuntu/ && chown -R ubuntu:ubuntu /home/ubuntu/.ssh/
 
 # Disable sudo password
 echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' | EDITOR='tee -a' visudo
