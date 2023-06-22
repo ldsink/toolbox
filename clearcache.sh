1b4c90198157a5b9cc86bd58ced7708d5a8652c9
ZHOU Cheng <c.zhou@live.com>
2023-06-22 17:44:01 +0800

    add crontab example

--- clearcache.sh
+++ clearcache.sh
@@ -4,3 +4,7 @@
 # 2 Clear dentries and inodes
 # 3 Clear pagecache, dentries, and inodes
 echo "echo 1 > /proc/sys/vm/drop_caches"
+
+# Set in Crontab for auto clear memory cache
+# For example, clear cache every 2 hours
+# 0 */2  *  *  * /path/to/clearcache.sh
