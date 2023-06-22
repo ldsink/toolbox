dc2cf112a8e731976af531116aaa7beff58d69bc
ZHOU Cheng <c.zhou@live.com>
2023-06-22 17:52:53 +0800

    set default to clear all

--- clearcache.sh
+++ clearcache.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# 1 Clear PageCache only
+# 1 Clear pagecache only
 # 2 Clear dentries and inodes
 # 3 Clear pagecache, dentries, and inodes
-echo "echo 1 > /proc/sys/vm/drop_caches"
+# Note, we are using "echo 3", but it is not recommended in production
+echo "echo 3 > /proc/sys/vm/drop_caches"
 
 # Set in Crontab for auto clear memory cache
 # For example, clear cache every 2 hours
